4a0f0897c04130cd24066e712be67634dafc6883
Cesar <cesarbaezmoya06@gmail.com>
2025-11-22 20:52:29 -0600

    agregado el metodo inverso

--- matrizFiles.java
+++ matrizFiles.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 public class matrizFiles {
 
@@ -37,5 +39,64 @@ public class matrizFiles {
         }
         return matrix;
     }
+    
+// escribe la matriz en un nuevoarchivo
+    public void writeMatrixToFile(String fileName, double[][] matriz){
+        try{
+            FileWriter file = new FileWriter("C:\\archivos\\" + fileName);
+            PrintWriter writer = new PrintWriter(file);
+            for(int i=0; i<matriz.length; i++){
+                for(int j=0; j<matriz[i].length; j++){
+                    writer.print(matriz[i][j] + " ");
+                }
+                writer.println();
+            }
+            file.close();
+        } catch(Exception e){
+            System.out.println("Error al escribir el archivo: " + e.toString());
+        }
+    }
 
+    // Calcula la inversa de la matriz 
+    public double[][] inverse(double[][] A){
+        int n = A.length;
+        double[][] aug = new double[n][2*n];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                aug[i][j] = A[i][j];
+            }
+            aug[i][i+n] = 1;
+        }
+
+        // metodo gauss Jordan23
+        for(int i=0; i<n; i++){
+            // Normalizamos fila i
+            double diag = aug[i][i];
+            if(diag == 0){
+                System.out.println("La matriz no es invertible.");
+                return null;
+            }
+            for(int j=0; j<2*n; j++){
+                aug[i][j] /= diag;
+            }
+            // Eliminamos otras filas
+            for(int k=0; k<n; k++){
+                if(k != i){
+                    double factor = aug[k][i];
+                    for(int j=0; j<2*n; j++){
+                        aug[k][j] -= factor * aug[i][j];
+                    }
+                }
+            }
+        }
+        // Extraemos la inversa
+        double[][] inv = new double[n][n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                inv[i][j] = aug[i][j+n];
+            }
+        }
+        return inv;
+    }
 }
